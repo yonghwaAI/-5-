b5c253f603bd0587bb0d777d4bf30d3eff762359
jhLee987 <20180782@sungshin.ac.kr>
2022-12-11 01:40:47 +0900

    모델 적용 전 로직 정리 & 코드, 주석 다듬기 & 필요없는 파일 삭제

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from strategy.RSIStrategy import *
+from strategy.Strategy import *
 import sys
 
 app = QApplication(sys.argv)
@@ -6,7 +6,7 @@ app = QApplication(sys.argv)
 # 데이터프레임 모든 컬럼 출력
 pd.set_option('display.max_columns', None)
 
-rsi_strategy = RSIStrategy()
-rsi_strategy.start()
+strategy = Strategy()
+strategy.start()
 
 app.exec_()
