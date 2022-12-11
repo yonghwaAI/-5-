18f9f7164a5e9860c872d3614f34496f139667ce
seoeun <20200941@sungshin.ac.kr>
2022-12-11 16:04:30 +0900

    매수매도전

--- test.py
+++ test.py
@@ -1,16 +1,7 @@
-import plotly.figure_factory as ff
-import plotly.graph_objects as go
-from flaml import AutoML
-automl = AutoML()
-from sktime.forecasting.model_selection import temporal_train_test_split
-
-from imblearn.over_sampling import RandomOverSampler
-from imblearn.combine import SMOTETomek
-
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
+automl = AutoML()
 
-from flaml.ml import sklearn_metric_loss_score
+'''from flaml.ml import sklearn_metric_loss_score
 from flaml.data import get_output_from_log
 import matplotlib.pyplot as plt
 
@@ -28,4 +19,4 @@ from sklearn.metrics import classification_report
 
 from flaml.ml import sklearn_metric_loss_score
 from flaml.data import get_output_from_log
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt'''
